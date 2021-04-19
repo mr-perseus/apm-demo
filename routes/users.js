b283f78cec555e6615f92e4e00f9150e9525d181
Jan <janfrei01@gmail.com>
2021-04-19 15:43:23 +0200

    sending an error

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   if (req.query.fail === "yes") {
-    res.status(500).send("this is supposed to fail");
+    next(new Error("this is supposed to fail"))
   } else {
     res.send('respond with a resource');
   }
